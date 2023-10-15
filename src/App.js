b2fa59a56e6e72fb42d1dd24e9abaf5bf5ce47b4
Daudu Arunegbode charles <blaqwayschannel@gmail.com>
2023-10-15 10:01:18 +0100

    created a login component with funtion to an api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
+import { BrowserRouter, Route, Routes, Navigate  } from 'react-router-dom';
 import './App.css';
+import Login from "./components/Login"
+import Dashboard from "./components/Dashboard"
+// import NavLogo from "./components/NavLogo"
 
 function App() {
   return (
-    <div className="App">
-      <h1>MY NEW PORTFOLIO WEBSITE</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        {/* <NavLogo/> */}
+      <Routes>
+          <Route path='/' element={<Navigate to="/login"/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+      </Routes>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
