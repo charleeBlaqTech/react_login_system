6de49bf64b24b9f4dc400a2ccb1d46b21908a190
Daudu Arunegbode charles <blaqwayschannel@gmail.com>
2023-10-16 05:24:41 +0100

    redirecting unspecified routes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           <Route path='/' element={<Navigate to="/login"/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/dashboard' element={<Dashboard/>}/>
-          <Route path='/*' element={<Navigate to="/login"/>}/>
+          <Route path='*' element={<Navigate to="/"/>}/>
       </Routes>
       </div>
     </BrowserRouter>
